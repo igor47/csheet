62fdf26f83f44c8f21da8d34e051677c06cb9cb9
Igor Serebryany <igor47@moomers.org>
2025-10-24 12:55:55 -0700

    test: attempt to diagnose flakey tests
    seem to only be failing on main...

--- src/middleware/requestLogging.ts
+++ src/middleware/requestLogging.ts
@@ -1,3 +1,4 @@
+import { config } from "@src/config"
 import { logger } from "@src/lib/logger"
 import { createMiddleware } from "hono/factory"
 import type { AuthVariables } from "./auth"
@@ -14,11 +15,22 @@ export const requestLoggingMiddleware = createMiddleware<{
   const start = Date.now()
   await next()
   const duration = Date.now() - start
-  logger.info("request", {
+
+  const data = {
     method: c.req.method,
     path: c.req.path,
     status: c.res.status,
     duration,
     user: c.get("user")?.email,
-  })
+  }
+
+  if (config.isProd) {
+    logger.info("request", data)
+  } else if (config.isTest) {
+    // nothing!
+  } else {
+    console.log(
+      `${data.method} ${data.path} - ${data.status} - ${data.duration}ms${data.user ? ` - user: ${data.user}` : ""}`
+    )
+  }
 })
